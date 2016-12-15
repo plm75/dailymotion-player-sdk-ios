5e6a1023e53cedc3da0c3b61af58e58763d1a030
Patrick Lam <patrick.lam@lambda-mobile.com>
2016-12-15 17:43:27 +0100

    Carthage support

---
+++ DailymotionPlayeriOS/DailymotionPlayeriOS.h
@@ -0,0 +1,19 @@
+//
+//  DailymotionPlayeriOS.h
+//  DailymotionPlayeriOS
+//
+//  Created by Patrick Lam on 15/12/2016.
+//  Copyright © 2016 Dailymotion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DailymotionPlayeriOS.
+FOUNDATION_EXPORT double DailymotionPlayeriOSVersionNumber;
+
+//! Project version string for DailymotionPlayeriOS.
+FOUNDATION_EXPORT const unsigned char DailymotionPlayeriOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DailymotionPlayeriOS/PublicHeader.h>
+
+
